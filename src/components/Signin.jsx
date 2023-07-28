be0b1b07dc91077a34e3b0473337ba21162258d4
iamfrankmunene <134502286+iamfrankmunene@users.noreply.github.com>
2023-07-28 14:23:57 +0300

    frontend

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -18,7 +18,7 @@ const SigninForm = ({ handleAuthenticationSuccess, showLogin }) => {
     e.preventDefault()
     try {
       // Call the backend API to sign in the user
-      const response = await axios.post('https://shelflife.onrender.com//register', {
+      const response = await axios.post('https://shelflife.onrender.com/register', {
         username,
         password,
       })
