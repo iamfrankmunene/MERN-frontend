2c1ee0263049aa6c7d296eb24704b50d990608ab
iamfrankmunene <134502286+iamfrankmunene@users.noreply.github.com>
2023-07-30 20:24:26 +0300

    personal project

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -18,7 +18,7 @@ const SigninForm = ({ handleAuthenticationSuccess, showLogin }) => {
     e.preventDefault()
     try {
       // Call the backend API to sign in the user
-      const response = await axios.post('https://mern-backend-nr1c5n5es-iamfrankmunene.vercel.app/register', {
+      const response = await axios.post('https://mern-backend-ashen.vercel.app/register', {
         username,
         password,
       })
